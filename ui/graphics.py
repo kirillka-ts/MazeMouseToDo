f8a1b0c2fbcdd73735ade99f2da4a4cb9e331922
irinaAvdeeva <irina.avdeeva@letovo.ru>
2024-03-04 13:00:41 +0300

    edit graphics.py

--- ui/graphics.py
+++ ui/graphics.py
@@ -25,6 +25,6 @@ def draw_image(image, x, y):
 
 # клиенты будут передавать координаты лабиринта
 def draw_circle(color, x, y, r):
-    #print("Здесь нужен pygame draw.circle с пересчетом на settings.tile_size")
+    """Здесь нужен pygame draw.circle с пересчетом на settings.tile_size"""
     pass
 
